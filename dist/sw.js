f6156e10137bca71f3871fd9771a065726a3f6b5
Tetragius <tetragius@gmail.com>
2021-09-29 16:43:29 +0300

    add pwa

--- dist/sw.js
+++ dist/sw.js
@@ -5,9 +5,19 @@ self.addEventListener('install', event => {
                 '/',
                 '/index.html',
                 '/index.css',
-                '/dist/',
                 '/dist/boundle.js',
             ]);
         })
     );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith((async () => {
+        const r = await caches.match(e.request);
+        if (r) return r;
+        const response = await fetch(e.request);
+        const cache = await caches.open(cacheName);
+        cache.put(e.request, response.clone());
+        return response;
+    })());
 });
